ed3b723c42831e4c11258d299cbda890bb355691
lorieZelev1722 <velezloremie046@gmail.com>
2024-12-19 10:35:08 +0800

    tailwind installation

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+// tailwind.config.js (ES module)
+export default {
+  content: [
+    './index.html',
+    './src/**/*.{js,jsx,ts,tsx}',  // Include React files
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
